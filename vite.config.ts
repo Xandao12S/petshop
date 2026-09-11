6a0eb2e3e12d7e7783aeb1e2562ec00d408984b2
Xandao12S <aalencar286@gmail.com>
2026-09-11 09:42:58 -0300

    Adiciona login e produtos ao build

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,20 @@ import { resolve } from "path";
 
 export default defineConfig({
   root: "src",
-
   server: {
     open: "/pages/index.html",
   },
-
   build: {
     outDir: "../dist",
     emptyOutDir: true,
-
     rollupOptions: {
       input: {
         main: resolve(import.meta.dirname, "src/pages/index.html"),
         admin: resolve(import.meta.dirname, "src/pages/admin.html"),
         servicos: resolve(import.meta.dirname, "src/pages/servicos.html"),
         contato: resolve(import.meta.dirname, "src/pages/contato.html"),
+        login: resolve(import.meta.dirname, "src/pages/login.html"),
+        produtos: resolve(import.meta.dirname, "src/pages/produtos.html"),
       },
     },
   },
